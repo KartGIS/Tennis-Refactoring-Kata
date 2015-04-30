e6e895a999aa6ffe469e92ec3b37f4e2fc2e6495
Roman Gural <rgural@kartgis.com.pl>
2015-04-30 12:29:03 +0200

    finished 2 and 3 TennisGames

--- java/src/main/java/AbstractTennisGame.java
+++ java/src/main/java/AbstractTennisGame.java
@@ -1,7 +1,46 @@
 public abstract class AbstractTennisGame implements TennisGame {
+    public static final int FIFTEEN_VALUE = 1;
+    public static final int FOURTY_VALUE = 3;
+
+    protected int P2point;
+    protected int P1point;
+    protected String player1Name;
+    protected String player2Name;
+
+    public AbstractTennisGame(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
+
+    protected String getScoreString(int score) {
+        if (score > FOURTY_VALUE)
+            return "";
+        return SCORES[score];
+    }
+
+    protected String leadingPlayerName() {
+        return ((P1point > P2point) ? player1Name : player2Name);
+    }
+
+    protected boolean differByOne() {
+        return Math.abs(P1point - P2point) == FIFTEEN_VALUE;
+    }
+
+    protected boolean allHaveLessOrEqualThanForty() {
+        return P2point <= FOURTY_VALUE && P1point <= FOURTY_VALUE;
+    }
+
+    protected boolean deuce() {
+        return tie() && P1point >= FOURTY_VALUE;
+    }
+
+    protected boolean tie() {
+        return P1point == P2point;
+    }
+
     public abstract void wonPoint(String playerName);
 
     public abstract String getScore();
 
-    protected final String[] SCORES = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+    protected final String[] SCORES = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; // 0,1,2,3
 }
--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -1,35 +1,37 @@
 
-public class TennisGame3 implements TennisGame {
-    
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+public class TennisGame3 extends AbstractTennisGame {
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    public TennisGame3(String player1Name, String player2Name) {
+        super(player1Name, player2Name);
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+        if (allHaveLessOrEqualThanForty() && !allHaveFourty()) {
+            return (tie())
+                    ? getScoreString(P1point) + "-All"
+                    : getScoreString(P1point) + "-" + getScoreString(P2point);
         }
+
+        if (tie()) {
+            return "Deuce";
+        }
+
+        return leadingType() + leadingPlayerName();
     }
-    
+
+    private String leadingType() {
+        return (differByOne() ? "Advantage ":  "Win for ");
+    }
+
+    private boolean allHaveFourty() {
+        return (P1point == FOURTY_VALUE && P2point == FOURTY_VALUE);
+    }
+
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            this.p1 += 1;
+            this.P1point += 1;
         else
-            this.p2 += 1;
+            this.P2point += 1;
         
     }
 
